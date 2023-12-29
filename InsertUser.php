7eb0e89a1895c312222ace37f6ffc862c0114ba7
Serg-Vas <balladaosoldate@gmail.com>
2023-12-29 14:43:52 +0200

    створити php backend script для POST запиту

---
+++ InsertUser.php
@@ -0,0 +1,39 @@
+<?php
+// Check if the request is a POST request
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require_once 'db_connection.php'; // Include the database connection script
+    print_r($_POST);
+    try {
+        // Establish database connection
+        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Get POST data from the form
+        $username = $_POST['name'];
+        echo $username;
+        $email = $_POST['email'];
+        // $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password for security
+        $address = $_POST['address'];
+        $phone_number = $_POST['phone'];
+
+        // Prepare the SQL statement to insert a user into the 'users' table
+        $stmt = $pdo->prepare("INSERT INTO users(username, password, address, phone_number, email) VALUES (:username, :password, :address, :phone_number, :email)");
+
+        // Bind parameters
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':phone_number', $phone_number);
+        $stmt->bindParam(':address', $address);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+
+        // Execute the SQL statement
+        $stmt->execute();
+
+        echo "User inserted successfully.";
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+} else {
+    echo "Invalid request method. This script accepts only POST requests.";
+}
+?>
