a790545832eaa667471c4bf5b667b06323634e1b
Serg-Vas <balladaosoldate@gmail.com>
2024-05-04 14:46:11 +0300

    svg manipulations

--- updateFood.php
+++ updateFood.php
@@ -5,18 +5,18 @@ update field in php mysql
 <?php
 try{
 require_once 'db_connection.php';
-$path = "D:\work\Food shop\back\images\\nachos.svg";
+$path = "D:\work\Food shop\back\images\\milkshake.svg";
 $id = 15;
-$name = 'Nachos';
-$price = 3.95;
-$restaurant_id = 5;
+// $name = 'Nachos';
+// $price = 3.95;
+// $restaurant_id = 5;
 $image = file_get_contents($path);
 // echo $image;
-$stmt = $pdo->prepare("UPDATE food SET name=:name, price=:price, restaurant_id=:restaurant_id, image=:image WHERE id=:id");
+$stmt = $pdo->prepare("UPDATE food SET  image=:image WHERE id=:id"); //name=:name, price=:price, restaurant_id=:restaurant_id,
 $stmt->bindParam(':id', $id);
-$stmt->bindParam(':name', $name);
-$stmt->bindParam(':price', $price);
-$stmt->bindParam(':restaurant_id', $restaurant_id);
+// $stmt->bindParam(':name', $name);
+// $stmt->bindParam(':price', $price);
+// $stmt->bindParam(':restaurant_id', $restaurant_id);
 $stmt->bindParam(':image', $image);
 
 $stmt->execute();
